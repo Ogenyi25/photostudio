81aaae4cf5871fe21ee1e783513df30a0cf8f40a
Ogenyi25 <johnogenyi4real@gmail.com>
2020-09-18 16:04:48 +0100

    changed styles for the navlinks

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,28 +7,39 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="logo">
-                <img src={require('../asset/images/logo.png')} alt="" className="page-logo"/>
+                <img src={require('../asset/images/logo.png')} alt="" className="page-logo" />
                 PhotoStudio
             </div>
             <ul>
                 <li className="nav-items">
-                    <Link to='/home' className="nav-link">Home</Link>
+                    <Link to='/home' className="nav-link">Home
+                    <hr />
+                    </Link>
+
                 </li>
 
                 <li className="nav-items">
-                    <Link to='/about-us' className="nav-link">About</Link>
+                    <Link to='/about-us' className="nav-link">About
+                    <hr className="about" />
+                    </Link>
                 </li>
 
                 <li className="nav-items">
-                    <Link to='/services' className="nav-link">Services</Link>
+                    <Link to='/services' className="nav-link">Services
+                    <hr className="services" />
+                    </Link>
                 </li>
 
                 <li className="nav-items">
-                    <Link to='/gallery' className="nav-link">Gallery</Link>
+                    <Link to='/gallery' className="nav-link">Gallery
+                    <hr className="gallery" />
+                    </Link>
                 </li>
-                
+
                 <li className="nav-items">
-                    <Link to='/contact-us' className="nav-link">Contact Us</Link>
+                    <Link to='/contact-us' className="nav-link">Contact Us
+                    <hr className="contact" />
+                    </Link>
                 </li>
             </ul>
             <input type="checkbox" id="check" />
